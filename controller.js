b29c2f12bc7b92858d7a034a0cf3db48cbf4587e
Robin Yap <robinyms78@gmail.com>
2025-04-16 16:01:32 +0800

    Implemented activity

--- controller.js
+++ controller.js
@@ -1,3 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// GET /home
+app.get('/home', (req, res) => {
+    res.send('This is the home page');
+});
+
+// POST /user
+app.post('/user', (req, res) => {
+    const { name, email, password } = req.body;
+
+    if ( !name || !email || !password) {
+        return res.status(400).json({ message: 'Missing name, email, or password' });
+    }
+
+    console.log(`User created: ${name}, ${email}`);
+    res.json({ message: `Received user ${name} with email ${email}`});
+});
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
@@ -5,3 +29,8 @@ function print(req, res){
 }
 
 module.exports = print;
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
